07845a2a8367ff7f528c6b99d501cbecc7f05a0c
Jacqui-MTZ <80400590+Jacqui-MTZ@users.noreply.github.com>
2021-11-23 16:20:36 -0600

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$host="localhost";
+$usuario="id16758287_martinezreyesjacquelinedolores";
+$pass="^Q0%Sh\2i-{Qf_=p";
+$nomdb="id16758287_bdagenda_martinezj";
+
+$conexion=new mysqli($host,$usuario,$pass,$nomdb);
+
+
+ 
+    ?>
